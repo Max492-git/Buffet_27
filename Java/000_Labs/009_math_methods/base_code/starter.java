d3db09efee4cccc224acf9daa7ab9919efd11646
Max492-git <maxbrop@outlook.com>
2026-09-18 18:03:14 +0000

    update

--- Java/000_Labs/009_math_methods/base_code/starter.java
+++ Java/000_Labs/009_math_methods/base_code/starter.java
@@ -9,6 +9,20 @@ class starter {
 	public static void main(String args[]) {
 		// the string "I love to learn coding remotely." will appear in
 		// the command window when you compile and run this program.
-		System.out.print("I love to learn coding remotely."); 
+		System.out.println(Math.max(13 - 6 * 11, 30 % 7 *(-2)));
+		 System.out.println(Math.sqrt(3 * 8 + 31 % 7));
+		 System.out.println(Math.pow(37 / 3, 35 % 21));
+		 System.out.println(Math.max(Math.pow(2,14%3),Math.sqrt(2*6)));
+
+		 Scanner sc = new Scanner(System.in);
+
+		 System.out.println("Please enter a double:");
+		 double num1 = sc.nextDouble();
+		 System.out.println("Please enter another double:");
+		 double num2 = sc.nextDouble();
+		 System.out.println("");
+		 System.out.println("Maximum number of x and y is:"+(Math.max(num1)+","+(num2)));
+
+
 	}
 }
--- Java/101_Lecture_Notes/LectureInput.java
+++ Java/101_Lecture_Notes/LectureInput.java
@@ -5,23 +5,42 @@ import java.util.Scanner;
 class LectureInput{
     public static void main(String args[]) {
         double item1 = 6.5;
+        
         double item2 = 7.25;
+        
         double item3 = 2.15;
+        
         System.out.println("Welcome to Max's Pizzaria");
+        
         System.out.println("");
+        
         System.out.print("Pepperoni pizza -");
+        
         System.out.println(item1);
+        
         System.out.print("Fettucine alfredo -");
+        
         System.out.println(item2);
+        
         System.out.print("Italian breadsticks -");
+        
         System.out.println(item3);
 
 
 
         System.out.println("How many Pepperoni Pizza would you like?");
-        Scanner st = new Scanner(System.in);
+        Scanner sc = new Scanner(System.in);
 
         int item1Quant = sc.nextInt();
+
+        System.out.println("How many Fettucine alfredo would you like");
+        
+        int item2Quant = sc.nextInt();
+
+        System.out.println("How many Italian breadsticks would you like");
+        
+        int item3Quant = sc.nextInt();
+
 
 	}
 }
